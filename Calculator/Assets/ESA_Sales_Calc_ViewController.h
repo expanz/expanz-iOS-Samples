f39d65281f73bc4e838691653136a17a58cc1e11
Jasper Blues <jasper.reloaded@gmail.com>
2011-12-12 11:48:23 +0800

    Add calculator sample.

---
+++ Calculator/Assets/ESA_Sales_Calc_ViewController.h
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  EXPANZ
+//  Copyright 2008-2011 EXPANZ
+//  All Rights Reserved.
+//
+//  NOTICE: Expanz permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#import <ExpanzIOS/expanz_ui_ActivityInstanceViewController.h>
+
+@interface ESA_Sales_Calc_ViewController : expanz_ui_ActivityInstanceViewController
+
+@property (nonatomic, retain) IBOutlet UILabel* ResultLabel;
+@property (nonatomic, retain) IBOutlet UILabel* Op1Label; 
+@property (nonatomic, retain) IBOutlet UILabel* Op2Label;
+@property (nonatomic, retain) IBOutlet UITextField* Result; 
+@property (nonatomic, retain) IBOutlet UITextField* Op1; 
+@property (nonatomic, retain) IBOutlet UITextField* Op2; 
+@property (nonatomic, retain) IBOutlet UIButton* add; 
+@property (nonatomic, retain) IBOutlet UIButton* subtract;
+@property (nonatomic, retain) IBOutlet UIButton* multiply;
+@property (nonatomic, retain) IBOutlet UIButton* divide; 
+
+
+- (IBAction) addClicked; 
+- (IBAction) subtractClicked; 
+- (IBAction) multiplyClicked; 
+- (IBAction) divideClicked; 
+
+@end
+
